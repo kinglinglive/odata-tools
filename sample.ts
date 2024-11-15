f4fded9c61de0bade531b94a95b74f11032137c0
kingling <kinglinglive@163.com>
2024-11-15 03:41:00 +0000

    feat: test update

--- sample.ts
+++ sample.ts
@@ -1,3 +1,12 @@
 export function sayHello(name: string): string {
   return `Hello ${name}`;
 }
+
+// 实现斐波那契数列
+export function fibonacci(n: number): number {
+  if (n === 0 || n === 1) {
+    return n;
+  }
+
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
